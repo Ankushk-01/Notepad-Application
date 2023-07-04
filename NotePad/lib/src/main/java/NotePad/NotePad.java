144c2cb75802e9f5a492ac1af4df94a340b509f0
Ankushk-01 <ankushkumar.me24@gmail.com>
2023-07-04 23:53:31 +0530

    I added the code in the project

--- NotePad/lib/src/main/java/NotePad/NotePad.java
+++ NotePad/lib/src/main/java/NotePad/NotePad.java
@@ -1,14 +1,16 @@
 package NotePad;
 
-public class NotePad {
-	
-	NotePad(){
-		
+import javax.swing.JFrame;
+
+public class NotePad extends JFrame {
+
+	NotePad() {
+
 	}
 
 	public static void main(String[] args) {
 		NotePad notepad = new NotePad();
-//		new NotePad();  // it is called enormous Object
+		// new NotePad(); // it is called enormous Object
 
 	}
 
