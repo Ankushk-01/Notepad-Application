622857deedd79816b8393a6d41514b7767c19a38
Ankush <129063140+Ankushk-01@users.noreply.github.com>
2023-07-04 23:40:14 +0530

    Add files via upload

---
+++ NotePad/lib/src/main/java/NotePad/NotePad.java
@@ -0,0 +1,15 @@
+package NotePad;
+
+public class NotePad {
+	
+	NotePad(){
+		
+	}
+
+	public static void main(String[] args) {
+		NotePad notepad = new NotePad();
+//		new NotePad();  // it is called enormous Object
+
+	}
+
+}
